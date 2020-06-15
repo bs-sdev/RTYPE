d6508d5ba996d3ffe9c2938868c7ad2d6035a3b7
Benjamin SOULAS <benjamin.soulas45@gmail.com>
2020-06-15 14:49:02 +0200

    Add Tiny README content and delete unused files

---
+++ python/README.md
@@ -0,0 +1,15 @@
+# R-TYPE Project
+
+## Aim
+
+Develop the same game as SEGA Master system's version. The sprites come from the web.
+This project has been started to discover how video games can be developed in Python code (with pygame library)
+
+## Requirements
+
+You must have python 3.7+ and ideally create a virtual environment in order to have necessary libraries only in this 
+project, not in you personal computer.
+
+## How to run the game
+
+@TODO
--- python/src/core/test_4/test_load_sprite_sheet.py
+++ python/src/core/test_4/test_load_sprite_sheet.py
@@ -6,33 +6,42 @@ from python.src.core.test_3.spritesheet import SpriteSheet
 
 pg.init()
 
+#
 clock = pg.time.Clock()
 
+# Window in which the game will be displayed in
 window = pg.display.set_mode((500, 500))
+
+# Background representing space in window's background
 bg = pg.image.load(os.path.join("../sprites", "SpaceBackground1.png"))
 
+# SpriteSheet class loading a gif image containing space ship sprite, coordinates will be used for each sprite
+# succession animation
 sp_sheet = SpriteSheet(os.path.join("../sprites", "r-typesheet1.gif"))
 
+# Sprite used while spaceship going right
 flyRight = [
     sp_sheet.get_image(x=167, y=2, width=32, height=15)
 ]
 
+# Sprite used while spaceship going left
 flyLeft = [
     sp_sheet.get_image(x=167, y=2, width=32, height=15)
 ]
 
+# Sprites used while spaceship going down
 flyDown = [
     sp_sheet.get_image(x=200, y=2, width=32, height=15),  # Down 1 inverse (soit up_1)
     sp_sheet.get_image(x=233, y=2, width=32, height=15)   # Down 2 inverse (soit up_2)
 ]
 
+# Sprites used while spaceship going up
 flyUp = [
     sp_sheet.get_image(x=134, y=2, width=32, height=15),  # Down 1 inverse (soit down_1)
     sp_sheet.get_image(x=101, y=2, width=32, height=15)  # Down 2 inverse (soit down_2)
 ]
 
-char = sp_sheet.get_image(x=167, y=2, width=32, height=15)
-
+# Sprites used while spaceship going right to show engine flames
 propellant = [
     sp_sheet.get_image(x=133, y=18, width=32, height=32),
     sp_sheet.get_image(x=166, y=18, width=32, height=32),
@@ -40,40 +49,33 @@ propellant = [
     sp_sheet.get_image(x=232, y=18, width=32, height=32)
 ]
 
+# Sprites used to show spaceship's firing
 beam = [
-    sp_sheet.get_image(x=213, y=85, width=17, height=12),
-    sp_sheet.get_image(x=231, y=85, width=17, height=12),
     sp_sheet.get_image(x=248, y=85, width=17, height=12),
-    sp_sheet.get_image(x=195, y=85, width=17, height=12)
+    sp_sheet.get_image(x=231, y=85, width=17, height=12),
+    sp_sheet.get_image(x=213, y=85, width=17, height=12)
 ]
 
-# x = 250
-# y = 250
-# width = 32
-# height = 15
-# vel = 5
-# is_jump = False
-# jump_count = 10
-# left = False
-# right = False
-# up = False
-# down = False
-# fire = False
-# fly_count = 0
-# propellant_count = 3
-# fire_count = 0
+# Sprite used when the ship is in stationary mode (no movement)
+char = sp_sheet.get_image(x=167, y=2, width=32, height=15)
 
 
 class Ship:
 
     def __init__(self, x, y, width, height):
+        """
+        Ship's constructor
+
+        :param x: ship's x coordinates
+        :param y: ship's y coordinates
+        :param width: ship's width
+        :param height: ship's height
+        """
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.vel = 5
-        self.is_jump = False
-        self.jump_count = 10
         self.left = False
         self.right = False
         self.up = False
@@ -83,25 +85,37 @@ class Ship:
         self.fire_count = 0
 
     def draw(self, win):
+        """
+        Function that draw the sprites on the given window depending of the action typed by the user.
+
+        Depending of the actions defined (left, right, up, down and no action), in the given window will be drawn the
+        right
+        :param win: Window in which sprites will be drawn with given algorithm and fly_count index (defining sprite to
+                    load in related array)
+        :return:
+        """
         if self.fly_count + 1 >= 3:
             self.fly_count = 0
 
+        # If going left, animate the going left's sprite (which is no animation)
         if self.left:
             win.blit(flyLeft[0], (self.x, self.y))
 
-        elif ship.right:
+        # If going right, animate the propellant showing flames of engine
+        elif self.right:
             win.blit(propellant[self.propellant_count], (self.x - 32, self.y - 12))
             win.blit(flyRight[0], (self.x, self.y))
-            if self.fly_count < 3:
-                self.fly_count += 1
-            else:
-                self.fly_count = 1
+            # if self.fly_count < 3:
+            #     self.fly_count += 1
+            # else:
+            #     self.fly_count = 1
 
             if self.propellant_count > 0:
                 self.propellant_count -= 1
             else:
                 self.propellant_count = 0
 
+        # If up, animate up's sprites
         elif self.up:
             win.blit(flyUp[self.fly_count], (self.x, self.y))
             # print("Up N° " + str(fly_count))
@@ -110,6 +124,7 @@ class Ship:
             else:
                 self.fly_count = 1
 
+        # If up, animate down's sprites
         elif self.down:
             win.blit(flyDown[self.fly_count], (self.x, self.y))
             # print("Down N° " + str(fly_count))
@@ -118,25 +133,43 @@ class Ship:
             else:
                 self.fly_count = 1
 
+        # If no action, load stationary position sprite
         else:
             win.blit(char, (self.x, self.y))
 
 
 class Projectile:
 
-    def __init__(self, x, y, radius, color):
+    def __init__(self, x, y, beam_index):
+        """
+        Beam's constructor
+
+        :param x: beam's x coordinate
+        :param y: beam's y coordinate
+        :param beam_index: beam's sprite index to load
+        """
         self.x = x
         self.y = y
-        self.radius = radius
-        self.color = color
+        # self.radius = radius
+        # self.color = color
         self.facing = 1
         self.vel = 8 * self.facing
+        self.beam_index = beam_index
 
     def draw(self, win):
-        pg.draw.circle(win, self.color, (self.x, self.y), self.radius, 1)
+        # pg.draw.circle(win, self.color, (self.x, self.y), self.radius, 1)
+        # load du sprite
+        # Il faut checker l'index pour supprimer les deux premiers sprite
+        win.blit(beam[self.beam_index], (self.x + 16, self.y - 7))
 
 
 def redraw_game_window(ship):
+    """
+    Redraw the ship in the window (global variable)
+
+    :param ship: Ship to redraw in window
+    :return: display updated
+    """
 
     window.blit(bg, (0, 0))
 
@@ -148,7 +181,12 @@ def redraw_game_window(ship):
     pg.display.update()
 
 
-# Main Loop
+########################################################################################################################
+#                                                                                                                      #
+#                                                                                                                      #
+#                                                   Main Loop                                                          #
+#                                                                                                                      #
+########################################################################################################################
 run = True
 ship = Ship(x=250, y=250, width=32, height=15)
 bullets = []
@@ -170,8 +208,14 @@ while run:
     keys = pg.key.get_pressed()
 
     if keys[pg.K_SPACE]:
-        if len(bullets) < 5:
-            bullets.append(Projectile(round(ship.x + ship.width // 2), round(ship.y + ship.height // 2), 6, (255, 255, 255), ))
+        # if len(bullets) < 5:
+        if len(bullets) < 3:
+            print("LEN OF BULLETS: " + str(len(bullets)))
+            # bullets.append(Projectile(round(ship.x + ship.width // 2), round(ship.y + ship.height // 2), 6, (255, 255, 255), ))
+            # Pour insérer les sprite dans le tableau à la place des cercles (radius et color), doivent être remplacer
+            # par les sprites de feu (3 sprites au total)
+            # bullets.append(Projectile(round(ship.x + ship.width // 2), round(ship.y + ship.height // 2), len(bullets)))
+            bullets.append(Projectile(round(ship.x + ship.width // 2), round(ship.y + ship.height // 2), 0))
 
     if keys[pg.K_LEFT] and ship.x > ship.vel:
         ship.x -= ship.vel
